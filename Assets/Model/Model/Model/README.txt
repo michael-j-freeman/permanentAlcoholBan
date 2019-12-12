eaaa1b05a1c2df6ec18c16e3985ac358d585ae8b
Flumdy <49465569+Flumdy@users.noreply.github.com>
2019-12-12 14:48:00 +1300

    player class skeleton developed in model

---
+++ Assets/Model/Model/Model/README.txt
@@ -0,0 +1,3 @@
+When using visual studio to add and and update files, do so outside of this 
+repository as the visual studio repository causes issues within this repository,
+then add them back in after adjsutments made.
--- Assets/Model/Model/Model/player.cs
+++ Assets/Model/Model/Model/player.cs
@@ -4,14 +4,36 @@
 public class Player
 {
 	public int drunk_level = 0; //out of 100
-	public int stamine = 100; ///goes down when used
+	public int stamine = 100; //goes down when used
+	public int sprint_speed = 50; //Increased after drinking and decreased when stamina is 0
 
-	static void Main(String[] args)
-    {
-		Player finn = new Player();
-		Console.WriteLine(finn.drunk_level);
+	public void update_drunk_level(Player player)
+		/*Increase player's drunk level by 10 unless at 100 already*/	
+	{
+		if (player.drunk_level < 90)
+		{
+			player.drunk_level += 10;
+		}
+		else
+		{
+			player.drunk_level = 100;
+		}
+	}
 
-		Console.ReadLine();
-    }
+	public void update_stamina(Player player)
+	/*Increase stamina upon drinking or waiting long enough,
+	 decrease upon sprinting*/
+	{
+
+	}
+
+	public void update_sprint_speed(Player player)
+	/*After drinking, sprint_speed is increased.
+	 If stamina is 0, sprint speed == walk speed (or maybe jsut over)*/
+	{
+
+	}
+
+
 
 }
