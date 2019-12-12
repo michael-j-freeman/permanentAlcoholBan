8218eb6b652912cc46d6c47d53846bc880d477c7
Flumdy <49465569+Flumdy@users.noreply.github.com>
2019-12-12 14:21:46 +1300

    added model

---
+++ Assets/Model/Model/Model/player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class Player
+{
+	public int drunk_level = 0; //out of 100
+	public int stamine = 100; ///goes down when used
+
+	static void Main(String[] args)
+    {
+		Player finn = new Player();
+		Console.WriteLine(finn.drunk_level);
+
+		Console.ReadLine();
+    }
+
+}
